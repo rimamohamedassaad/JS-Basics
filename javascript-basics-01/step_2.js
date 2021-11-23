1ed5352b8ee075095856d6b5b1dba8db5d2a93a2
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-23 22:03:48 +0200

    01-step2

---
+++ javascript-basics-01/step_2.js
@@ -0,0 +1,7 @@
+/*var name="";
+var surname="";
+var city=""*/
+var person = ["Doe", "John", "New York"];
+window.alert("                                      " +"name : " + person[0] + "\n" +
+             "                                      " + "surname : " + person[1] + "\n" +
+             "                                      " + "city : " + person[2]);
