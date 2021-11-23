a827b4cbbbb014dec0142a086823116fb455bbdf
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-23 22:23:56 +0200

    01-step4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+var name = window.prompt("what is your name ");
+var surName = window.prompt("what is your surname ");
+var city = window.prompt("where do you live ");
+window.alert("                                      " +"hello  " + name+ " " + surName + "\n" +
+            
+             "                                      " + "nice that you're from  " + city);
