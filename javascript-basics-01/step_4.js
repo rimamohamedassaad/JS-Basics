e110b3d6c0f77ce843ca5b5daac9cdf22fef73ac
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-24 20:53:48 +0200

    01-step5

--- javascript-basics-01/step_4.js
+++ javascript-basics-01/step_4.js
@@ -2,5 +2,5 @@ var name = window.prompt("what is your name ");
 var surName = window.prompt("what is your surname ");
 var city = window.prompt("where do you live ");
 window.alert("                                      " +"hello  " + name+ " " + surName + "\n" +
-            
-             "                                      " + "nice that you're from  " + city);
+
+             "                                      " + "nice that you're from  " + city); 
---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,3 @@
+var number1 = prompt("enter the first number");
+var number2 = prompt("enter the second number");
+alert ("the result of "+ number1 +" * " + number2 +" = " + number1 * number2);
