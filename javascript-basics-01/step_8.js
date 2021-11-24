b4f7fdff9fb62905294cbe23165b4e52d72de640
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-24 22:51:50 +0200

    01-step8(2)

--- javascript-basics-01/step_8.js
+++ javascript-basics-01/step_8.js
@@ -1,9 +1,20 @@
-var age = prompt("how old ar you");
+/*var age = prompt("how old ar you");
 if (age > 18){
    alert("you are over 18");
 }
 else {
      alert("you are under 18");
+}*/
+function ageIf(){
+    var age = document.getElementById('age').value;
+
+    if (age > 18){
+        alert("you are over 18");
+     }
+     else {
+          alert("you are under 18");
+     }
+    alert(result);
 }
     
     
