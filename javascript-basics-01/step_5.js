56c58c184e7f70bd221857f63e5695467e58b3b4
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-24 21:45:22 +0200

    01-step5(2)

--- javascript-basics-01/step_5.js
+++ javascript-basics-01/step_5.js
@@ -1,3 +1,10 @@
-var number1 = prompt("enter the first number");
+/*var number1 = prompt("enter the first number");
 var number2 = prompt("enter the second number");
-alert ("the result of "+ number1 +" * " + number2 +" = " + number1 * number2);
+alert ("the result of "+ number1 +" * " + number2 +" = " + number1 * number2);*/
+var number1;
+var number2
+function showResult() {
+  number1 = document.getElementById('first_number').value;
+  number2 = document.getElementById('second_number').value;
+  alert("the result of "+ number1 +" * " + number2 +" = " + number1 * number2);
+} 
