4920e895c378fc0fd2f10a24dfe69d4b9106b710
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-24 22:14:00 +0200

    01-step6(2)

--- javascript-basics-01/step_6.js
+++ javascript-basics-01/step_6.js
@@ -1,12 +1,11 @@
 
-var number1 = prompt('enter the first number');
+/*var number1 = prompt('enter the first number');
 var number2 = prompt('enter the second number');
-alert("the result of "+ number1 +" / " + number2 +" = " + number1 / number2);
-/*var number1;
+alert("the result of "+ number1 +" / " + number2 +" = " + number1 / number2);*/
+var number1;
 var number2
-function division() {
+function divisionReminder() {
   number1 = document.getElementById('first_number').value;
   number2 = document.getElementById('second_number').value;
-  alert("the result of "+ number1 +" / " + number2 +" = " + number1 / number2);
+  alert("the reminder of the division : "+ number1 +" / " + number2 +" = " + number1 % number2);
 } 
-*/
