b7728d29b5aed7e746ab90213758235fc0d36491
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-24 22:39:17 +0200

    01-step7(2)

--- javascript-basics-01/step_7.js
+++ javascript-basics-01/step_7.js
@@ -1,4 +1,4 @@
-var shoe = prompt("what is you shoe size");
+/*var shoe = prompt("what is you shoe size");
 var birth = prompt("what is your bith year");
 function someMath(){
     var multiple = shoe * 2;
@@ -7,8 +7,8 @@ function someMath(){
     var substract = multiple50 - birth ;
     var result = substract + 1766;
     alert(result);
-}
-/*function someMath(){
+}*/
+function someMath(){
     var shoe = document.getElementById('shoe_size').value;
 var birth = document.getElementById('year').value;
     var multiple = shoe * 2;
@@ -17,4 +17,4 @@ var birth = document.getElementById('year').value;
     var substract = multiple50 - birth ;
     var result = substract + 1766;
     alert(result);
-}*/
+}
