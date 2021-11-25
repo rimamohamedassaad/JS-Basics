a014fec689a20adef3082e98a6617f63efd55db7
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-25 22:08:01 +0200

    02-step1

---
+++ javascript-basics-02/step 1/step_1.js
@@ -0,0 +1,9 @@
+document.querySelector('#image1').addEventListener("mouseover", changeImage);
+document.querySelector('#image1').addEventListener("mouseout", returnImage);
+function changeImage(){
+document.querySelector('#image1').src="images/image1_2.jpg";
+}
+function returnImage(){
+    document.querySelector('#image1').src="images/image1.jpg";
+    }
+   
