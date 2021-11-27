255a5f0ba12569bc66b79455102c160af2f16c27
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-11-27 12:58:19 +0200

    03-step3

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,17 @@
+
+function changeColor(color){
+    switch (color){
+        case 'green' :
+            document.getElementById('text').style.color = 'green';
+            break;
+    
+    case 'red':
+        document.getElementById('text').style.color = 'red';
+            break;
+            case 'blue':
+                document.getElementById('text').style.color = 'blue';
+            break;
+            default:
+       }   
+
+}
